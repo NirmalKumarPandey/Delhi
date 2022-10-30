88226e6df206a1e930f336dbcd8f67a2dceeb2b7
nirmal kumar <nirmalnirman36@gmail.com>
2022-10-30 12:39:10 +0530

    This is Second Commit

---
+++ app/src/main/java/com/example/mvvmandretrofitandinterseptor/GitHubChecking.java
@@ -0,0 +1,14 @@
+package com.example.mvvmandretrofitandinterseptor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GitHubChecking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_hub_checking);
+    }
+}
