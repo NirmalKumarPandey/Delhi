ef232dff0456c46b44d76928854cf755064b4e24
nirmal kumar <nirmalnirman36@gmail.com>
2022-11-02 07:44:58 +0530

    This is Fourth Commit

--- app/src/main/java/com/example/mvvmandretrofitandinterseptor/GitHubChecking.java
+++ app/src/main/java/com/example/mvvmandretrofitandinterseptor/GitHubChecking.java
@@ -8,13 +8,14 @@ import android.widget.TextView;
 
 public class GitHubChecking extends AppCompatActivity
 {
-            private TextView mSum;
+            private TextView mSum,mMul;
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_git_hub_checking);
         mSum=findViewById(R.id.txt);
+        mMul=findViewById(R.id.txt_sum);
         int a=20;
         int b=3;
         int s=addData(a,b);
@@ -23,7 +24,19 @@ public class GitHubChecking extends AppCompatActivity
         mSum.setText(s);*/
       /*  Boolean data=addData(a,b);
         mSum.setText(""+data);*/
+        int c = 0,d = 0;
+        double mul=multiplication(c,d);
+        mMul.setText(String.valueOf(mul));
 
+
+    }
+
+    private double multiplication(int c, int d)
+    {
+        c=30;
+        d=50;
+        double mul=c*d;
+        return mul;
 
     }
 
